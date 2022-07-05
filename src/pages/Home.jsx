d2d2a30bdcb273d496aefbd9bc596ba50fbe178c
mustafaalhm <mustafaalhm@gmail.com>
2022-07-05 09:10:06 -0700

    6/7/2022

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,31 @@ import Helmet from '../components/Helmet/Helmet';
 import heroImg from '../assets/images/hero.png'
 import { Link } from 'react-router-dom';
 import Category from '../components/Ui/Category/Category';
+import '../Styles/home.css'
+
+// import img data
+import featureImg01 from '../assets/images/service-01.png'
+import featureImg02 from '../assets/images/service-02.png'
+import featureImg03 from '../assets/images/service-03.png'
+
+const featureData = [
+  {
+    title: 'Quicke Delivery',
+    imgUrl: featureImg01,
+    desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, voluptatum?'
+  },
+  {
+    title: 'Super dine Up',
+    imgUrl: featureImg02,
+    desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, voluptatum?'
+  },
+  {
+    title: 'Easy pick up',
+    imgUrl: featureImg03,
+    desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, voluptatum?'
+  },
+
+]
 const Home = () => {
   return (
     <Helmet title='Home'>
@@ -39,8 +64,37 @@ const Home = () => {
         </Container>
 
       </section>
+      <section className='pt-0'>
+        <Category />
+      </section>
+
       <section>
-        <Category/>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h5 className='feature__subtitle mb-4'>What We Serve?</h5>
+              <h2 className='feature__title'>Just Sit Back At Home</h2>
+              <h2 className='feature__title'>we will <span>take care</span></h2>
+              <p className='mb-1 bt-4  feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, vero?</p>
+              <p className=' feature__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, vero?</p>
+
+            </Col>
+            {
+              featureData.map((item, index) => (
+                <Col lg='4' md='4' key={index} className='mt-4'>
+                  <div className='feature__item text-center px-5 py-3'>
+                    <img src={item.imgUrl} alt=""  className='w-25 mb-3'/>
+                    <h5 className='fw-bold mb-3'>{item.title}</h5>
+                    <p>{item.desc}</p>
+                  </div>
+                </Col>
+              ))
+
+            }
+
+
+          </Row>
+        </Container>
       </section>
     </Helmet>
   )
